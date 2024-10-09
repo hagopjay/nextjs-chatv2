21a697415b44bfe2d81817fbfbc0645fa4802592
hagopjay <hagopjay@gmail.com>
2024-10-09 02:13:19 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -75,26 +75,7 @@ export function Header() {
           />
         </a>
       
-        /*
-        <a
-          target="_blank"
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
-          rel="noopener noreferrer"
-          className={cn(buttonVariants({ variant: 'outline' }))}
-        >
-          <IconGitHub />
-          <span className="hidden ml-2 md:flex">GitHub</span>
-        </a>
-        <a
-          href="https://vercel.com/templates/Next.js/nextjs-ai-chatbot"
-          target="_blank"
-          className={cn(buttonVariants())}
-        >
-          <IconVercel className="mr-2" />
-          <span className="hidden sm:block">Deploy to Vercel</span>
-          <span className="sm:hidden">Deploy</span>
-        </a>
-        */
+        
         
       </div>
 
